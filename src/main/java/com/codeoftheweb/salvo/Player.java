752d0871404de0caecd267ba6f1b61ead5af5bd6
HiufaiLau <hiuailau.41@gmail.com>
2018-12-02 18:06:38 +0100

    added player and player repository class

--- src/main/java/com/codeoftheweb/salvo/Player.java
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -1,5 +1,86 @@
 package com.codeoftheweb.salvo;
+import javax.persistence.*;
 
-public class Player {
-    
+@Entity
+    public class Player<onetoMany> {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")//???????
+        private long id;
+        private String userName;
+
+        @onetoMany(fetch = FetchType.EAGER)//???????????
+        @JoinColumn(name="owner_id")
+        private Player owner;
+
+        private String email;
+        private String password;
+
+
+        public Player() {}
+
+        public Player (String userName, String email, String password) {
+            this.userName = userName;
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+         public long getId() {
+        return id;
+        }
+
+        public void setId(long id) {
+             this.id = id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String toString() {
+            return userName;
+        }
+
+        public Player getOwner() {
+        return owner;
+        }
+
+        public void setOwner(Player owner) {
+        this.owner = owner;
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.codeoftheweb.salvo;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findByLastName(String lastName);
+}
